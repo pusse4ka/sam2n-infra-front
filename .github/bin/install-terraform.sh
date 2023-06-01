cae7b7a99eb557a6ec35c108231a858d8991309a
Nastya Bakhshieva <nastyabakhshieva@coherentsolutions.com>
2023-06-02 00:36:53 +0400

    SAM-26: Add terraform to deploy FE
    ________________________________________
    
    What: Configure GitHub workflow for pull requests
    
    Why: we want to check pull requests before they got merged

---
+++ .github/bin/install-terraform.sh
@@ -0,0 +1,4 @@
+tf_version="$TERRAFORM_VERSION"
+wget https://releases.hashicorp.com/terraform/"$tf_version"/terraform_"$tf_version"_linux_amd64.zip
+unzip terraform_"$tf_version"_linux_amd64.zip
+sudo mv terraform /usr/local/bin/
