3df9a23fb9cf81ef3e7e41c8f309689fbdf2f698
Nastya Bakhshieva <nastyabakhshieva@coherentsolutions.com>
2023-06-20 02:26:48 +0400

    fix script

--- composite_actions/setup_tf/install_tf.sh
+++ composite_actions/setup_tf/install_tf.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-echo "$version"
+while getopts ":v:" opt; do
+  case $opt in
+    v)
+      tf_version="$OPTARG"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
 
-if [ -z "$version" ]; then
-    echo -e "\nPlease pass --version <version> to run this command!\n"
-    exit 1
+if [ -z "$tf_version" ]; then
+  echo -e "\nPlease pass -v <version> to run this command!\n"
+  exit 1
 fi
 
-tf_version="$version"
+#if [ -z "$version" ]; then
+#    echo -e "\nPlease pass --version <version> to run this command!\n"
+#    exit 1
+#fi
+
+#tf_version="$version"
 wget https://releases.hashicorp.com/terraform/"$tf_version"/terraform_"$tf_version"_linux_amd64.zip
 unzip terraform_"$tf_version"_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
