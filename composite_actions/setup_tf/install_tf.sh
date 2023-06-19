a3d2d502499821be7a944b23e40dc739372e1019
Nastya Bakhshieva <nastyabakhshieva@coherentsolutions.com>
2023-06-20 02:29:06 +0400

    fix aws script

--- composite_actions/setup_tf/install_tf.sh
+++ composite_actions/setup_tf/install_tf.sh
@@ -16,12 +16,6 @@ if [ -z "$tf_version" ]; then
   exit 1
 fi
 
-#if [ -z "$version" ]; then
-#    echo -e "\nPlease pass --version <version> to run this command!\n"
-#    exit 1
-#fi
-
-#tf_version="$version"
 wget https://releases.hashicorp.com/terraform/"$tf_version"/terraform_"$tf_version"_linux_amd64.zip
 unzip terraform_"$tf_version"_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
